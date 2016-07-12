7586134f5cd8cbfc53c8732d4611533d5f64cfa0
Joel Kuiper <me@joelkuiper.eu>
2016-07-12 23:26:52 +0200

    Indentation

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-kdtree "1.2.0" :exclusions [org.clojure/clojure]]
-                 [info.debatty/java-lsh "0.9"]
-                 ])
+                 [info.debatty/java-lsh "0.9"]])
