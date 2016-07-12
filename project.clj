401fb2f700d73cb9aeedc0aacfc6cc6cc2b84e92
Joel Kuiper <me@joelkuiper.eu>
2016-07-13 01:35:16 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-similar "0.1.0"
+(defproject clj-similar "0.1.1"
   :description "Fast similar set lookup using MinHash and K-d trees"
   :url "https://github.com/vortext/clj-similar"
   :license {:name "Eclipse Public License"
